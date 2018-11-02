7a4e654da549feb7d1577299d4c49fc3600b7449
Artem Glukhanko <v1z.workmail@gmail.com>
2018-11-02 21:49:28 +0300

    design review

--- app/blocks/menu/index.js
+++ app/blocks/menu/index.js
@@ -1,25 +1,10 @@
 import $ from 'jquery';
 
-const $projectsToggler = $('.js-toggle-projects');
-const $projectsList = $('.js-projects-list');
-const $projectsTogglerIcon = $('.js-projects-toggler-icon');
-const $artsToggler = $('.js-toggle-arts');
-const $artsList = $('.js-arts-list');
-const $artsTogglerIcon = $('.js-arts-toggler-icon');
-
-const openSubListClass = 'menu__sub-nav-list_open';
-const revertedTogglerIconClass = 'menu__toggler-icon_revert';
+const $listToggler = $('.js-list-toggler');
+const openTogglerState = 'menu__list-toggler_open';
 
 export default class Menu {
 	init() {
-		$projectsToggler.on('click', () => {
-			$projectsList.toggleClass(openSubListClass);
-			$projectsTogglerIcon.toggleClass(revertedTogglerIconClass);
-		});
-
-		$artsToggler.on('click', () => {
-			$artsList.toggleClass(openSubListClass);
-			$artsTogglerIcon.toggleClass(revertedTogglerIconClass);
-		});
+		$listToggler.on('click', function () { $(this).toggleClass(openTogglerState); });
 	}
 }
