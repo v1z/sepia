30a8ced8bf337c8fb307b539975e86a8c4adedee
Artem Glukhanko <v1z.workmail@gmail.com>
2018-11-02 23:35:23 +0300

    header scroll logic

--- app/blocks/header/index.js
+++ app/blocks/header/index.js
@@ -4,13 +4,41 @@ const $page = $('.page');
 const $header = $('.header');
 const $toggler = $('.js-menu-toggler');
 
+const topMenuClass = 'header_top-menu';
+const fixedMenuClass = 'header_fixed-menu';
+
 export default class Header {
+	constructor() {
+		this.oldScroll = '';
+		this.isHeaderFixed = false;
+	}
+
 	handleMenuToggle() {
 		$header.toggleClass('header_menu-open');
 		$page.toggleClass('page_menu-open');
 	}
 
+	handlePageScroll() {
+		// handle header fix to top
+		if (window.scrollY === 0) {
+			$header.addClass(topMenuClass);
+		}else {
+			$header.removeClass(topMenuClass);
+		}
+
+		// handle header show/hide
+		this.isHeaderFixed = this.oldScroll > window.scrollY;
+		this.oldScroll = window.scrollY;
+
+		if (this.isHeaderFixed) {
+			$header.addClass(fixedMenuClass);
+		}else {
+			$header.removeClass(fixedMenuClass);
+		}
+	}
+
 	init() {
 		$toggler.on('click', this.handleMenuToggle);
+		window.addEventListener('scroll', this.handlePageScroll);
 	}
 }
