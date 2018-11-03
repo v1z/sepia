aef881262133a9797e08cb787bc99f9d1fa5bf59
Artem Glukhanko <v1z.workmail@gmail.com>
2018-11-03 21:55:19 +0300

    menu behaviour

--- app/blocks/header/index.js
+++ app/blocks/header/index.js
@@ -6,29 +6,52 @@ const $toggler = $('.js-menu-toggler');
 
 const topMenuClass = 'header_on-top';
 const fixedMenuClass = 'header_fixed';
+const crossIntroMenuClass = 'header_cross-intro';
 
 export default class Header {
 	constructor() {
 		this.oldScroll = '';
 		this.isHeaderFixed = false;
+		this.isMenuOpen = false;
+		this.scrollBeforeMenuOpen = '';
+		this.debounce = true;
 	}
 
 	handleMenuToggle() {
+		if (!this.isMenuOpen) {
+			this.scrollBeforeMenuOpen = window.pageYOffset;
+			$header.css({transition: 'transform .5s, color .3s'});
+		}
+
 		$header.toggleClass('header_menu-open');
 		$page.toggleClass('page_menu-open');
+
+		this.isMenuOpen = !this.isMenuOpen;
+
+		if (!this.isMenuOpen) {
+			window.scrollTo(0, this.scrollBeforeMenuOpen);
+			setTimeout(() => { $header.css({transition: 'transform .5s, color .3s, background-color .3s'}); }, 10);
+		}
 	}
 
 	handlePageScroll() {
 		// handle header fix to top
-		if (window.scrollY === 0) {
+		if (window.pageYOffset <= 200) {
 			$header.addClass(topMenuClass);
 		}else {
 			$header.removeClass(topMenuClass);
 		}
 
+		// handle cross intro section
+		if ($('#intro')[0].getBoundingClientRect().bottom <= 0 + $header.height() + 100) {
+			$header.addClass(crossIntroMenuClass);
+		}else {
+			$header.removeClass(crossIntroMenuClass);
+		}
+
 		// handle header show/hide
-		this.isHeaderFixed = this.oldScroll > window.scrollY;
-		this.oldScroll = window.scrollY;
+		this.isHeaderFixed = this.oldScroll > window.pageYOffset;
+		this.oldScroll = window.pageYOffset;
 
 		if (this.isHeaderFixed) {
 			$header.addClass(fixedMenuClass);
@@ -39,6 +62,6 @@ export default class Header {
 
 	init() {
 		$toggler.on('click', this.handleMenuToggle);
-		window.addEventListener('scroll', this.handlePageScroll);
+		window.addEventListener('scroll', () => this.handlePageScroll());
 	}
 }
